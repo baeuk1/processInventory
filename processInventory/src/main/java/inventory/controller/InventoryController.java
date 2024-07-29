0da6c7d839acf2a1b555a2d2358a895919eec6e2
baeukchoi <baeuk2004@naver.com>
2024-07-29 15:31:43 +0900

    [재고차감API] Test Commit

---
+++ processInventory/src/main/java/inventory/controller/InventoryController.java
@@ -0,0 +1,32 @@
+package inventory.controller;
+
+import cm.dto.api.ResponseDto;
+import inventory.dto.VariableRequest;
+import inventory.service.InventoryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/inventory/linkage")
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @ApiOperation("재고차감 수신")
+    @PostMapping("/reduceInventory")
+    public ResponseDto<?> reduceInventory(
+            @ApiParam("재고차감 파라미터") @RequestBody @Valid final VariableRequest request) {
+
+    	inventoryService.reduceInventory(request.toEntity());
+        return new ResponseDto<>(HttpStatus.OK.value(), "Success");
+    }
+}
